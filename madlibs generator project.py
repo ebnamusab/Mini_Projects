2ac6af3ee3f4d76e614cd8dfd6132681b83e701d
Ebna Musab <126104057+ebnamusab@users.noreply.github.com>
2023-06-20 23:53:16 +0600

    Add files via upload

---
+++ madlibs generator project.py
@@ -0,0 +1,12 @@
+Name = input("Enter Your Name: ")
+Profession = input("Enter your profession here: ")
+Institution = input("Enter your Institute Name: ")
+Noun = input("Enter welcome place: ")
+Verb = input("Enter a verb: ")
+Noun1 = input("enter your topic: ")
+Noun3 = input("Enter a noun: ")
+
+# madlibs = "My name is "+Name+".I am a "+Profession+" of "+Institution+".Welcome to our "+Noun+".Here, we "+Verb+" about "+Noun1+" and have fun with "+Noun3+" "
+
+madlibs = f"My name is {Name}.I am a {Profession} of {Institution}.Welcome to our {Noun}.Here, we {Verb} about {Noun1} and have fun with {Noun3} "
+print(madlibs)
